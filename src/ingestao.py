4155010499b3280e87dea1009485e19211a5cb5f
Marcelo <m4rceloalves@gmail.com>
2025-10-26 22:11:57 -0300

    Fix overlap

--- src/ingestao.py
+++ src/ingestao.py
@@ -55,7 +55,7 @@ def dividir_documentos(documentos):
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
-        chunk_overlap=200,
+        chunk_overlap=150
         length_function=len,
         separators=["\n\n", "\n", " ", ""]
     )
